397fab1241b458f63138664a0dddc59f9685e03d
fly <frrodrigueza@gmail.com>
2024-04-03 17:31:20 -0500

    Initial Commit

---
+++ FarmersFridgeCodeChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FarmersFridgeCodeChallenge
+//
+//  Created by Fabian  Rodriguez on 3/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
