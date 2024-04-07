9d9b3d1cb48f84605fb7892dbe9b74b28fc44fd4
fly <frrodrigueza@gmail.com>
2024-04-07 12:33:03 -0500

    [FarmersFridgeCodeChallenge] Refactor over StemmingViewModel in order to hide refresh view

--- FarmersFridgeCodeChallenge/View Controllers/StemmingViewController/View Models/StemmingViewModel.swift
+++ FarmersFridgeCodeChallenge/View Controllers/StemmingViewController/View Models/StemmingViewModel.swift
@@ -50,35 +50,17 @@ final class StemmingViewModel {
         }
         
         dataStorage.write(inputStemWords)
-        
-//        if let storedStemWords = dataStorage.read() {
-//            
-//            /// Update storedSteamWords
-//            var storedSteamWordsUpdated = storedStemWords.map { (storedStemWord: StemWordData) in
-//                if let stemWordToUpdate = inputsStemWords.first(where: { storedStemWord.id == $0.id })  { /// If element needs to be updated
-//                    /// update storeSteamWord with inputStoredWord
-//                    return StemWordData(
-//                        stemWord: storedStemWord.stemWord,
-//                        occurrance: storedStemWord.occurrance + stemWordToUpdate.occurrance,
-//                        inflectionalWords: storedStemWord.inflectionalWords +  stemWordToUpdate.inflectionalWords
-//                    )
-//                }
-//                
-//                return storedStemWord  /// Stored stem word didn't need to be updated
-//            }
-//            
-//            /// New storedSteamWord
-//            let newStemWords = inputsStemWords.filter { stem in !storedStemWords.contains(where: { stem.id == $0.id }) }
-//            storedSteamWordsUpdated.append(contentsOf: newStemWords)
-//            
-//            dataStorage.write(storedSteamWordsUpdated)
-//        }
-        
+        refreshInputSteamWords(inputStemWords)
+    }
+}
+
+private extension StemmingViewModel {
+    func refreshInputSteamWords(_ inputStemWords: [StemWordData]) {
+        /// read input stem words with posibles updates
         let updatedInputStemWords = dataStorage.read()?.filter { storedStemWord in
             inputStemWords.contains(where: {$0.id == storedStemWord.id})
         }
         
         updatedInputStemWords.flatMap{self.stemWordsData = $0}
-    
     }
 }
